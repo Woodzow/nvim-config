b60d17a2646a03ebc93e14142a4a6b33c24d984a
Woodzow <wuzhongwz23@gmail.com>
2026-01-19 16:46:16 +0800

    rewrite the treesiter-plugins-settings

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,12 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    opts = {
-        ensure_installed = { "lua", "toml", "vim", "bash", "c", "cpp", "python", "java", "json" },
-        highlight = {enable = true },
-    }
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "lua", "toml", "vim", "bash", "c", "cpp", "python", "java", "json" },
+    highlight = { enable = true },
+  },
+  config = function(_, opts)
+    require'nvim-treesitter'.setup(opts)
+  end,
 }
+

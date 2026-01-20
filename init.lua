8f6693dc57ab2be1f63d7eef26ef7d2921e3a07b
Woodzow <wuzhongwz23@gmail.com>
2026-01-20 20:00:49 +0800

    adjust and beautify the dashboard

--- init.lua
+++ init.lua
@@ -1,25 +1,9 @@
+--关于bufferline的黑条一直显示的问题
+--是由于lazy，也就是插件在option之后才加载，从而会被bufferline插件在默认设置为showtabline = 2始终显示
+--从而可以直接将这一个配置写在这个文件末尾（加载插件之后）--vim.opt.showtabline = 1(两个及以上显示)
+--或者在bufferline配置中加入always_show_bufferline = false,
+--也可以写一个自动命令，让打开dashboard的时候才不现实
 require("core.options")
 require("core.keymaps")
 require("core.lazy")
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+require("core.basic")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -13,10 +13,8 @@ opt.autoindent = true
 --防止包裹
 -- opt.warp = false
 
-
 --光标行
 opt.cursorline = true
-
 
 --启用鼠标
 opt.clipboard:append("unnamedplus")
@@ -36,17 +34,26 @@ opt.signcolumn = "yes"
 
 opt.showmode = false
 
+-- 先将默认状态设为 0 (隐藏)
+vim.opt.showtabline = 0
 
+--设置只有dashboard界面不显示tabline
+-- 创建一个自动命令组
+vim.api.nvim_create_autocmd("BufEnter", {
+	nested = true,
+	callback = function()
+		-- 获取当前文件的类型
+		local ft = vim.bo.filetype
+		-- 获取当前 buffer 的名字
+		local bn = vim.api.nvim_buf_get_name(0)
 
-
-
-
-
-
-
-
-
-
-
-
-
+		-- 逻辑判断：
+		-- 如果当前是 Alpha 仪表盘，或者没有文件名（空界面），则隐藏
+		if ft == "alpha" or ft == "dashboard" or bn == "" then
+			vim.opt.showtabline = 0
+		else
+			-- 只要打开了实际的文件，就显示 tabline
+			vim.opt.showtabline = 2
+		end
+	end,
+})

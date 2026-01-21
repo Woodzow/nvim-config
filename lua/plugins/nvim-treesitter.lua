33bb69ea8268b9b64ce5d47a6bd1f76fd6040014
Woodzow <wuzhongwz23@gmail.com>
2026-01-21 22:47:54 +0800

    add noice plugins

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "toml", "vim", "bash", "c", "cpp", "python", "java", "json" },
-    highlight = { enable = true },
-  },
-  config = function(_, opts)
-    require'nvim-treesitter'.setup(opts)
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "lua", "toml", "vim", "bash", "c", "cpp", "python", "java", "json" },
+		highlight = { enable = true },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
+	end,
 }
-
